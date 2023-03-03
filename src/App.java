fad6a551eb9cd5f29428de8f1021dacc036b6c65
Pablo Nguyen <pnguy150@calpoly.edu>
2023-03-03 15:11:22 -0800

    Initial Committ

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class App extends JFrame {
 
 
-    public static void main(String... args){
+    public static void main(String[] args){
         App app = new App();
         app.setSize(500,500);
         app.setVisible(true);
