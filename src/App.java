f72f436c264a56106292c6267637090be540784f
Pablo Nguyen <pnguy150@calpoly.edu>
2023-03-03 15:11:11 -0800

    Initial Committ

--- src/App.java
+++ src/App.java
@@ -1,2 +1,16 @@
-public class App {
+import javax.swing.*;
+
+public class App extends JFrame {
+
+
+    public static void main(String... args){
+        App app = new App();
+        app.setSize(500,500);
+        app.setVisible(true);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public App(){
+
+    }
 }
--- src/MainController.java
+++ src/MainController.java
@@ -1,2 +1,46 @@
-public class MainController {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MainController implements MouseListener, MouseMotionListener, ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
 }
